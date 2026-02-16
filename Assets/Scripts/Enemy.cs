05d42ca94e639e7ca9c790bdc4eea5d8d9864c2a
No2feraotu <no2feraotu@gmail.com>
2026-02-16 14:26:22 -0600

    fixed attack anim and added death for dummy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,18 +6,44 @@ public class Enemy : MonoBehaviour
     public Animator anim;
     public Health health;
 
+    [Header("Death Fx")]
+    [SerializeField] private GameObject[] deathParts;
+    [SerializeField] private float spawnForce = 5;
+    [SerializeField] private float torque = 5;
+    [SerializeField] private float lifeTime = 2;
+
     private void OnEnable()
     {
         health.OnDamaged += HandleDamage;
+        health.OnDeath += HandleDeath;
     }
 
     private void OnDisable()
     {
         health.OnDamaged -= HandleDamage;
+        health.OnDeath -= HandleDeath;
     }
 
     void HandleDamage()
     {
         anim.SetTrigger("isDamaged");
+    }
+
+    void HandleDeath()
+    {
+        foreach (GameObject prefab in deathParts)
+        { 
+            Quaternion rotaion = Quaternion.Euler(0,0,Random.Range(0.5f, 1)).normalized;
+           GameObject part =  Instantiate(prefab, transform.position, rotaion);
+
+            Rigidbody2D rb = part.GetComponent<Rigidbody2D>();
+
+            Vector2 radomDirection = new Vector2(Random.Range(-1, 1), Random.Range(0.5f, 1)).normalized;
+            rb.linearVelocity = radomDirection * spawnForce;
+            rb.AddTorque(Random.Range(-torque, torque),ForceMode2D.Impulse);
+
+            Destroy(part, lifeTime);
+        }
+        Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/Player/Combat.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Combat : MonoBehaviour
+{
+    [Header("Attack Settings")]
+    public int damage;
+    public float attackRadius = 0.5f;
+    public float attackCooldown = 1.5f;
+    public Transform attackPoint;
+    public LayerMask enemyLayer;
+    public Animator hitFX;
+
+    public Player player;
+
+    public bool CanAttack => Time.time >= nextAttackTime;
+    public float nextAttackTime;
+
+    public void AttackAnimationFinished()
+    {
+        player.AttackAnimationFinished();
+    }
+
+    public void Attack()
+    {
+        if (!CanAttack)
+        {
+            return;        
+        }
+
+        nextAttackTime = Time.time + attackCooldown;
+
+        Collider2D enemy = Physics2D.OverlapCircle(attackPoint.position, attackRadius, enemyLayer);
+
+        if (enemy != null)
+        {
+            hitFX.Play("HitFX");
+            enemy.gameObject.GetComponent<Health>().ChangeHealth(-damage);
+        }
+    }
+}
